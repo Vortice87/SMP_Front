04ef23bfe4626544bd77646953359f253eb50ff9
Vortice R <v.u.bedmar@gmail.com>
2018-08-21 23:48:25 +0200

    se comprime en base 64 el input type file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 // import the services
 import { AuthenticationService } from './services/authentication/authentication.service';
@@ -54,6 +55,7 @@ import { ModalModule } from 'ngx-bootstrap';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     ModalModule.forRoot(),
   ],
--- src/app/components/home/sourcing/list-request/request-details/upload-cv/upload-cv.component.ts
+++ src/app/components/home/sourcing/list-request/request-details/upload-cv/upload-cv.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Cv } from '../../../../../../models/cv';
 
+import {ElementRef, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 @Component({
   selector: 'app-upload-cv',
   templateUrl: './upload-cv.component.html',
@@ -9,20 +12,85 @@ import { Cv } from '../../../../../../models/cv';
 })
 export class UploadCvComponent implements OnInit {
 
+  // form: FormGroup;
+  // loading: boolean = false;
+
+  // private cv: Cv;
+  // public requestId: number;
+  // public miarchivo: Blob;
+
+
+  // ngOnInit() {
+  //   this.cv = new Cv(null, this.requestId, '', new Date(), null, '', '' );
+  // }
+
+  // @ViewChild('fileInput') fileInput: ElementRef;
+
+  // constructor(private fb: FormBuilder,
+  //   public bsModalRef: BsModalRef
+  // ) {
+  //   this.createForm();
+  // }
+
+  // createForm() {
+  //   this.form = this.fb.group({
+  //     name: ['', Validators.required],
+  //     avatar: null
+  //   });
+  // }
+
+  // onFileChange(event) {
+  //   let reader = new FileReader();
+  //   if(event.target.files && event.target.files.length > 0) {
+  //     let file = event.target.files[0];
+  //     reader.readAsDataURL(file);
+  //     reader.onload = () => {
+  //       this.form.get('avatar').setValue({
+  //         filename: file.name,
+  //         filetype: file.type,
+  //         value: reader.result.split(',')[1]
+  //       })
+  //     };
+  //   }
+  // }
+
+  // onSubmit() {
+  //   const formModel = this.form.value;
+  //   this.loading = true;
+
+  //   setTimeout(() => {
+  //     console.log(formModel);
+  //     alert('done!');
+  //     this.loading = false;
+  //   }, 1000);
+  // }
+
+  // clearFile() {
+  //   this.form.get('avatar').setValue(null);
+  //   this.fileInput.nativeElement.value = '';
+  // }
+
   private cv: Cv;
   public requestId: number;
-  public miarchivo: File;
+  public miarchivo: any;
 
-  constructor(
-    public bsModalRef: BsModalRef
-  ) { }
 
   ngOnInit() {
     this.cv = new Cv(null, this.requestId, '', new Date(), null, '', '' );
   }
 
-  uploadFile(): void {
-    console.log(this.miarchivo.size);
+  constructor(public bsModalRef: BsModalRef
+  ) { }
+
+  onFileChange(event) {
+    let reader = new FileReader();
+
+      let file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+         this.miarchivo = reader.result.split(',')[1]
+         console.log(this.miarchivo);
+      };
   }
 
 }
